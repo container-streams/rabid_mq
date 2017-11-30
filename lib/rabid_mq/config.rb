041eb27168af656f1a14acc575ed17911711ec59
Tyrone <tdubs241083@gmail.com>
2017-11-30 08:15:07 +0200

    Merge defaults with custom config

--- lib/rabid_mq/config.rb
+++ lib/rabid_mq/config.rb
@@ -2,10 +2,14 @@ module RabidMQ
   module Config
     class << self
       def load_config
-        @config ||= YAML.load(ERB.new(File.read(file_name)).result)[Rails.env].symbolize_keys
+        @config ||= default_config.merge custom_config
       rescue Errno::ENOENT, NameError => e
         puts "[WARN] #{e.message} in #{__FILE__}. Falling back to default config"
         default_config
+      end
+
+      def custom_config
+        YAML.load(ERB.new(File.read(file_name)).result)[Rails.env].symbolize_keys || {}
       end
 
       def default_config
@@ -18,7 +22,8 @@ module RabidMQ
           :pass           => "guest",
           :heartbeat      => :server, # will use RabbitMQ setting
           :frame_max      => 131072,
-          :auth_mechanism => "PLAIN"
+          :auth_mechanism => "PLAIN",
+          :recover_from_connection_close => false
         }
       end
 
